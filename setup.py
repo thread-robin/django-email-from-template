66b5d03d45c36a03816bfb893b6f1dd960a59238
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-07 09:36:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name='django-email-from-template',
     description="Send emails generated entirely from Django templates.",
     version='0.1',
-    url='http://code.playfire.com/',
+    url='https://chris-lamb.co.uk/projects/django-email-from-template',
 
-    author='Playfire.com',
-    author_email='tech@playfire.com',
-    license='BSD',
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+    license="BSD",
 
     packages=find_packages(),
     package_data={'': [
