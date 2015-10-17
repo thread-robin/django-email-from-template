668e1525a9a602330a006303563005c6b2bd5814
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-17 22:45:55 +0100

    `lru_cache` decorator is now a class, thus requiring a __call__

--- email_from_template/utils.py
+++ email_from_template/utils.py
@@ -1,12 +1,12 @@
-from django.utils.lru_cache import lru_cache
+from django.utils import lru_cache
 
 from . import app_settings
 
-@lru_cache
+@lru_cache.lru_cache()
 def get_render_method():
     return from_dotted_path(app_settings.EMAIL_RENDER_METHOD)
 
-@lru_cache
+@lru_cache.lru_cache()
 def get_context_processors():
     return [from_dotted_path(x) for x in app_settings.EMAIL_CONTEXT_PROCESSORS]
 
