fcc6e37526ce25beb118b861c44cdc10616e311c
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-24 16:59:49 +0800

    fork

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,8 @@
 project = 'django-email-from-template'
 version = ''
 release = ''
-copyright = '2010, 2011 UUMC Ltd.'
+copyright = '2016 Chris Lamb'
 
-html_logo = 'playfire.png'
 html_theme = 'nature'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 html_title = "%s documentation" % project
@@ -11,6 +10,6 @@ master_doc = 'index'
 exclude_trees = ['_build']
 templates_path = ['_templates']
 latex_documents = [
-  ('index', '%s.tex' % project, html_title, u'Playfire', 'manual'),
+  ('index', '%s.tex' % project, html_title, u'Chris Lamb', 'manual'),
 ]
 intersphinx_mapping = {'http://docs.python.org/': None}
